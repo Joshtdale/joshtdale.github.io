ca8d35edba63d9d2670244378c0fdf0f17a7c48b
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-01 20:57:13 +0000

    usestate

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
+
+  const [page, setPage] = useState('Home')
+
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Navbar setPage={setPage}/>
+      {page == 'Home' && <Home />}
     </div>
   );
 }

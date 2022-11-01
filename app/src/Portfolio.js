ab0d1c77db2fc32bb74175a6411acece6ee1b99b
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-01 23:07:58 +0000

    updates

--- app/src/Portfolio.js
+++ app/src/Portfolio.js
@@ -1,20 +1,19 @@
 import React from 'react'
 
-export default function Portfolio() {
+function Portfolio() {
   return (
-    <div class="row m-5">
-    <div class="col portCard">
-        <div class="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-            style="background-image: url('/images/tic-tac-toe.png');">
-            <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+    <div className="row m-5">
+    <div className="col portCard">
+        <div className="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+            <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                 
-                <ul class="d-flex list-unstyled mt-auto">
-                    <li class="me-auto">
-                        <a href="https://5500-joshtdale-tictactoe-6dwwaoasjxs.ws-us70.gitpod.io/index.html"><button class="btn btn-secondary">View project</button></a>
+                <ul className="d-flex list-unstyled mt-auto">
+                    <li className="me-auto">
+                        <a href="https://5500-joshtdale-tictactoe-6dwwaoasjxs.ws-us70.gitpod.io/index.html"><button className="btn btn-secondary">View project</button></a>
                     </li>
                     
-                    <li class="d-flex align-items-center">
-                        <svg class="bi me-2" width="1em" height="1em">
+                    <li className="d-flex align-items-center">
+                        <svg className="bi me-2" width="1em" height="1em">
                             <use xlink:href="#calendar3"></use>
                         </svg>
                         <small>10/07/22</small>
@@ -26,21 +25,21 @@ export default function Portfolio() {
 
 
 
-    <div class="col portCard">
-        <div class="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+    <div className="col portCard">
+        <div className="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
         style="background-image: url('/images/mind-reader.png');">
-        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+        <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
             
-            <ul class="d-flex list-unstyled mt-auto">
-                <li class="me-auto">
-                    <a href="https://joshtdale.github.io/mind-reader/"><button class="btn btn-secondary">View project</button></a>
+            <ul className="d-flex list-unstyled mt-auto">
+                <li className="me-auto">
+                    <a href="https://joshtdale.github.io/mind-reader/"><button className="btn btn-secondary">View project</button></a>
                 </li>
                 
-                <li class="d-flex align-items-center">
-                    <svg class="bi me-2" width="1em" height="1em">
+                <li className="d-flex align-items-center">
+                    <svg className="bi me-2" width="1em" height="1em">
                         <use xlink:href="#calendar3"></use>
                     </svg>
-                    <small class="text-dark">10/07/22</small>
+                    <small className="text-dark">10/07/22</small>
                 </li>
             </ul>
         </div>
@@ -49,18 +48,18 @@ export default function Portfolio() {
 
     
 
-    <div class="col portCard">
-        <div class="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+    <div className="col portCard">
+        <div className="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
         style="background-image: url('/images/alarm-clock.png');">
-        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+        <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
             
-            <ul class="d-flex list-unstyled mt-auto">
-                <li class="me-auto">
-                    <a href="https://joshtdale.github.io/alarm-clock/"><button class="btn btn-secondary">View project</button></a>
+            <ul className="d-flex list-unstyled mt-auto">
+                <li className="me-auto">
+                    <a href="https://joshtdale.github.io/alarm-clock/"><button className="btn btn-secondary">View project</button></a>
                 </li>
                 
-                <li class="d-flex align-items-center">
-                    <svg class="bi me-2" width="1em" height="1em">
+                <li className="d-flex align-items-center">
+                    <svg className="bi me-2" width="1em" height="1em">
                         <use xlink:href="#calendar3"></use>
                     </svg>
                     <small>10/07/22</small>
@@ -71,19 +70,19 @@ export default function Portfolio() {
     </div>
 </div>
 
-<div class="row m-5">
-    <div class="col portCard">
-        <div class="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+<div className="row m-5">
+    <div className="col portCard">
+        <div className="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
             style="background-image: url('/images/scary-story-generator.png');">
-            <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                 
-                <ul class="d-flex list-unstyled mt-auto">
-                    <li class="me-auto">
-                        <a href="https://joshtdale.github.io/silly-story-generator/sandbox/sandbox.html"><button class="btn btn-secondary">View project</button></a>
+                <ul className="d-flex list-unstyled mt-auto">
+                    <li className="me-auto">
+                        <a href="https://joshtdale.github.io/silly-story-generator/sandbox/sandbox.html"><button className="btn btn-secondary">View project</button></a>
                     </li>
                     
-                    <li class="d-flex align-items-center">
-                        <svg class="bi me-2" width="1em" height="1em">
+                    <li className="d-flex align-items-center">
+                        <svg className="bi me-2" width="1em" height="1em">
                             <use xlink:href="#calendar3"></use>
                         </svg>
                         <small>10/07/22</small>
@@ -95,21 +94,20 @@ export default function Portfolio() {
 
 
 
-    <div class="col portCard">
-        <div class="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-        style="background-image: url('/images/thundercat.png');">
-        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+    <div className="col portCard">
+        <div className="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+        <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
             
-            <ul class="d-flex list-unstyled mt-auto">
-                <li class="me-auto">
-                    <a href="https://dale-webpage-project.glitch.me/index.html"><button class="btn btn-secondary">View project</button></a>
+            <ul className="d-flex list-unstyled mt-auto">
+                <li className="me-auto">
+                    <a href="https://dale-webpage-project.glitch.me/index.html"><button className="btn btn-secondary">View project</button></a>
                 </li>
                 
-                <li class="d-flex align-items-center">
-                    <svg class="bi me-2" width="1em" height="1em">
+                <li className="d-flex align-items-center">
+                    <svg className="bi me-2" width="1em" height="1em">
                         <use xlink:href="#calendar3"></use>
                     </svg>
-                    <small class="text-light">10/07/22</small>
+                    <small className="text-light">10/07/22</small>
                 </li>
             </ul>
         </div>
@@ -118,18 +116,18 @@ export default function Portfolio() {
 
     
 
-    <div class="col portCard">
-        <div class="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+    <div className="col portCard">
+        <div className="tic-tac-toe card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
         style="background-image: url('/images/weather-app.png');">
-        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+        <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
             
-            <ul class="d-flex list-unstyled mt-auto">
-                <li class="me-auto">
-                    <a href="https://5500-joshtdale-weatherapp-x9bdgvdqca8.ws-us70.gitpod.io/index.html"><button class="btn btn-secondary">View project</button></a>
+            <ul className="d-flex list-unstyled mt-auto">
+                <li className="me-auto">
+                    <a href="https://5500-joshtdale-weatherapp-x9bdgvdqca8.ws-us70.gitpod.io/index.html"><button className="btn btn-secondary">View project</button></a>
                 </li>
                 
-                <li class="d-flex align-items-center">
-                    <svg class="bi me-2" width="1em" height="1em">
+                <li className="d-flex align-items-center">
+                    <svg className="bi me-2" width="1em" height="1em">
                         <use xlink:href="#calendar3"></use>
                     </svg>
                     <small>10/07/22</small>
@@ -139,5 +137,5 @@ export default function Portfolio() {
     </div>
     </div>
 </div>
-  )
-}
+)
+};

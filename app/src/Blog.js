19e3c067c868bd314d187e4e8e0c49f9a606090b
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-06 21:53:47 +0000

    blog5 QA

---
+++ app/src/Blog.js
@@ -0,0 +1,21 @@
+import BlogSprint4 from "./blogs/BlogSprint4"
+import BlogSprint3 from "./blogs/BlogSprint3"
+import BlogSprint5 from "./blogs/BlogSprint5"
+import BlogSprint6 from "./blogs/BlogSprint6"
+import BlogSprint7 from "./blogs/BlogSprint7"
+
+function Blog(){
+    return (
+        <div>
+            {/* <BlogSprint3 /> */}
+            {/* <BlogSprint4 /> */}
+            
+            <BlogSprint5 />
+            <BlogSprint6 />
+            <BlogSprint7 />
+
+        </div>
+    )
+}
+
+export default Blog
---
+++ app/src/blogs/BlogSprint4.js
@@ -0,0 +1,48 @@
+function BlogSprint4(){
+    return (
+        <div className="container">
+
+        <div className="row justify-content-center text-center">
+            {/* <div className="col"></div> */}
+            <div className="col-10 mt-5 card bg-dark bg-opacity-50">
+                <h1>Intro to ReactJS</h1>
+                <p>This week was our first week with React. It was a rocky start, at first it seemed like we spent all this time learning JavaScript 
+                    only to be confused by how similar yet different React is. Early on in the week, it all just magically clicked for me. 
+                This led to me being able to make large strides this week in terms of React concepts and being able to add stretch goals to my project.</p>
+
+            </div>
+        </div>
+
+        <div className="row justify-content-center">
+            <div className="col-10 m-5">
+                <h3 className="text-center mb-4">Weekly Q&A</h3>
+                <ul>
+                    <li>
+                        <h5>Do you think JavaScript Frameworks and Libraries are easy or hard to use? Why?</h5>
+                        <p>I feel like this was possibly my best week of the bootcamp. For some reason React just clicked
+                            for me earlier in the week so I was able to spend more time
+                            putting things into practice as opposed to struggling with the basics of React.</p>
+                    </li>
+
+                    <li>
+                        <h5>What Frameworks or Libraries are you interested in learning more about?</h5>
+                        <p>There aren't any frameworks off the top of my head that I know enough about to think that they
+                            are something that I would want
+                            to learn. However, I'm really enjoying React so far and I'm excited to learn more of it's
+                            capabilities.</p>
+                    </li>
+
+                    <li>
+                        <h5>Find one emerging JavaScript tool that you would like to learn more about or use in your
+                            final project.</h5>
+                            <p>I think local storage seems to be an extremely useful tool that can enhance the user experience</p>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default BlogSprint4

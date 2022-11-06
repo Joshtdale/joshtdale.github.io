75688655671eb5d161fb7cbbb453d690ec21cfef
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-06 21:58:55 +0000

    :

--- app/src/blogs/BlogSprint6.js
+++ app/src/blogs/BlogSprint6.js
@@ -2,7 +2,53 @@ import React from 'react'
 
 function BlogSprint6() {
   return (
-    <div>Blog-sprint-6</div>
+    <>
+        <div className="row justify-content-center text-center">
+            {/* <div className="col"></div> */}
+            <div className="col-10 mt-5">
+                <h1>The halfway mark</h1>
+                <p></p>
+
+            </div>
+        </div>
+
+        <div className="row justify-content-center">
+            <div className="col-10 m-5 card bg-dark bg-opacity-50">
+                <h3 className="text-center mb-4">Weekly Q&A</h3>
+                <ul>
+                    <li>
+                        <h5>What have you learned about CLIs? Do you think they are scary?</h5>
+                        <p>CLIs can be a big help in helping the user get around within an operating system. If you know your
+                            way around a CLI, you can quickly input commands, parameters, and call functions to accomplish tasks
+                            much quicker than trying to fumble around the interface of your os (operating system). I so far have a 
+                            very limited understanding of CLIs and pretty much only know how to use it for commits, pushes, and 
+                            branch creation. In the future I would like to spend more time learning how to become a CLI wizard 
+                            but at the moment it seems like a whole other language to me.
+                        </p>
+                    </li>
+
+                    <li>
+                        <h5>If you were to build a CLI, what would it do?</h5>
+                        <p>Since I'm basically a baby when it comes to my CLI knowledge and vocabulary, I don't know that I'm in any real
+                            position to answer this question. Maybe I would make things dumbed down a bit so that some of the commands 
+                            are a little more intuitive. Although I'm sure the creators of each different CLI and people with a lot of 
+                            experience think that it is super intuitive.
+                        </p>
+                    </li>
+
+                    <li>
+                        <h5>List some ideas that without a Framework, would take you a long time to code</h5>
+                            <p>Frameworks add a level of abstraction to your code so that there are less meticulous things for 
+                                you to worry about. They simplify many things that would other wise be complicated. For instance, React 
+                                gives you the ability to build elements and reuse them. This saves a ton of time when you are building
+                                webpages because you only have to build things like a navbar once for example.
+                            </p>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        </>
   )
 }
 

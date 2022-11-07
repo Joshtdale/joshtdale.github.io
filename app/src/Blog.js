4f1db7f1fc591508140452cd7602762aac4a1a1d
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-07 19:51:51 +0000

    teaser blog element

--- app/src/Blog.js
+++ app/src/Blog.js
@@ -4,15 +4,30 @@ import BlogSprint5 from "./blogs/BlogSprint5"
 import BlogSprint6 from "./blogs/BlogSprint6"
 import BlogSprint7 from "./blogs/BlogSprint7"
 
-function Blog(){
+function BlogTeaser() {
+    return (
+        <div className="row">
+            <div className="col p-4 d-flex flex-column bg-dark bg-opacity-50 position-static m-5">
+                <strong className="d-inline-block mb-2 text-primary">World</strong>
+                <h3 className="mb-0">Featured post</h3>
+                <div className="mb-1 text-muted">Nov 12</div>
+                <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="stretched-link">Continue reading</a>
+            </div>
+        </div>
+    )
+}
+
+function Blog() {
     return (
         <div>
+            <BlogTeaser />
             {/* <BlogSprint3 /> */}
             {/* <BlogSprint4 /> */}
-            
-            <BlogSprint5 />
+
+            {/* <BlogSprint5 />
             <BlogSprint6 />
-            <BlogSprint7 />
+            <BlogSprint7 /> */}
 
         </div>
     )

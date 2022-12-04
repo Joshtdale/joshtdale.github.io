e7dd8130b0a1249d7744170f45d08382d3339d3b
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-12-04 18:23:19 +0000

    hash router fix

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Portfolio from './Portfolio';
 import Blog from './Blog';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
@@ -12,13 +12,25 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/portfolio' element={<Portfolio />}/>
-          <Route path='/blog' element={<Blog />}/>
-        </Routes>
-      </Router>
+      {/* <Router> */}
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={<Home/>}
+        />
+        <Route
+          exact
+          path='/portfolio'
+          element={<Portfolio/>}
+        />
+        <Route
+          exact
+          path='/blog'
+          element={<Blog/>}
+        />
+      </Routes>
+      {/* </Router> */}
 
     </div>
   );
